138876504632eb77c98086e1e41bc166b8fa1a59
guidiamond <guilherme.batista770@gmail.com>
2021-05-02 12:44:16 -0300

    new assembler with classes working (needs testing)

--- Assembly/assembler.py
+++ Assembly/assembler.py
@@ -3,15 +3,44 @@ import re
 from typing import List, Dict
 
 
+class Formater:
+    @staticmethod
+    def gen_opcode_dict(n_bits: str, lista_opcodes: List[str]):
+        opcode_dict = {}
+        for idx, opc_name in enumerate(lista_opcodes):
+            op_code_bits = f"{idx:04b}"
+            opcode_dict[opc_name] = op_code_bits
+        return opcode_dict
+
+    @staticmethod
+    def gen_imediato_ram(label_name, labels_dict: Dict[str, int]):
+        posicao_mem = labels_dict[label_name]
+        return f"{posicao_mem:012b}"
+
+    @staticmethod
+    def gen_imediato_valor(imediato_str: str):
+        imediato_num = int(imediato_str)
+        return f"{imediato_num:08b}"
+
+    @staticmethod
+    def gen_imediato_rom(label_name, labels_dict: Dict[str, int]):
+        posicao_mem = labels_dict[label_name]
+        return f"{posicao_mem:010b}"
+
+
 class Assembler:
-    lista_opcodes: List[str]
+    opcodes_dict: Dict[str, int]
     raw_assembly_lines: List[str]
     fmt_assembly_lines: List[str]
     labels_dict: Dict[str, int]
+    jump_instructions: List[str]
 
-    def __init__(self, code: List[str], lista_opcodes: List[str]):
+    def __init__(
+        self, code: List[str], lista_opcodes: List[str], jump_instructions: List[str]
+    ):
         self.raw_assembly_lines = code
-        self.lista_opcodes = lista_opcodes
+        self.opcodes_dict = Formater.gen_opcode_dict(4, lista_opcodes)
+        self.jump_instructions = jump_instructions
 
     def pre_pro(self):
         fmt_code = []
@@ -27,6 +56,7 @@ class Assembler:
             linha = linha.rstrip()
             # remove line breaks
             linha = re.sub("\n", "", linha)
+            linha = re.sub(",", "", linha)
             # dont add empty lines
             if len(linha) == 0:
                 lines_skipped += 1
@@ -43,8 +73,48 @@ class Assembler:
         self.fmt_assembly_lines = fmt_code
         self.labels_dict = labels_dict
 
+    @staticmethod
+    def gen_line_tokens(line: str):
+        return line.split()
+
+    def get_instruction_type(self, tokens: List[str]):
+        if tokens[0][0] in self.jump_instructions:
+            return "a"  # ROM (10b)
+        elif tokens[1][0] == "$":
+            return "b"  # IMEDIATO_VALOR (8b)
+        else:
+            return "c"  # RAM (12b)
+
+    def parse_line(self, line: str):
+        tokens = self.gen_line_tokens(line)  # ex: [MOV, $1]
+        instruction = list(f"{0:016b}")  # ex: [0,0,0...] representing the bits
+        opcode_name = tokens[0]  # ex: MOV
+
+        opcode_bin = self.opcodes_dict[opcode_name]
+        instruction[12:15] = opcode_bin
+
+        inst_type = self.get_instruction_type(tokens)
+
+        if inst_type == "a":
+            imediato_rom_bin = Formater.gen_imediato_rom(tokens[1], self.labels_dict)
+            instruction[:9] = imediato_rom_bin
+
+        elif inst_type == "b":
+            token_imediato_valor = tokens[1]
+            imediato_valor = token_imediato_valor[
+                1:
+            ]  # ex: $1 (token_imediato) -> 1 (imediato_valor)
+            imediato_bin = Formater.gen_imediato_valor(imediato_valor)
+            instruction[0:7] = imediato_bin
+
+        elif inst_type == "c":
+            imediato_ram_bin = Formater.gen_imediato_ram(tokens[1], self.labels_dict)
+            instruction[:11] = imediato_ram_bin
+
     def parse(self):
         self.pre_pro()
+        for line in self.fmt_assembly_lines:
+            self.parse_line(line)
 
 
 def main(raw_assembly: List[str]):
@@ -60,7 +130,8 @@ def main(raw_assembly: List[str]):
         "ADD",
         "SUB",
     ]
-    Assembler(raw_assembly, lista_opcodes).parse()
+    jump_instructions = ["JE", "JMP", "JL"]
+    Assembler(raw_assembly, lista_opcodes, jump_instructions).parse()
 
 
 if __name__ == "__main__":
